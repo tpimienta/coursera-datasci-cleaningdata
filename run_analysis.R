ff40ac3b8d5b51ce517162c30c84d0e7f55f4e3c
tom <tom@phpfunk.com>
2017-02-25 18:16:06 -0800

    Replace read.table() w/ faster data.table::fread()

--- run_analysis.R
+++ run_analysis.R
@@ -2,11 +2,12 @@
 
 library(plyr)
 library(dplyr)
+library(data.table)
 
 # information that is common to processing both training and test data sets
 
 # the  variables represented in each column of the data frames
-features <- read.table('uci/features.txt', comment.char='', stringsAsFactors=F)
+features <- fread('uci/features.txt')
 
 # only want variables that are mean and standard deviation
 idx_of_mean_and_std <- grep("mean|std", features$V2, ignore.case=T)
@@ -14,8 +15,17 @@ idx_of_mean_and_std <- grep("mean|std", features$V2, ignore.case=T)
 # a logical vector is required to filter the features to only the mean and std dev
 logical_idx_of_mean_and_std<-grepl("mean|std", features$V2, ignore.case=T)
 
+# fix names on the data frame 
+# here using logical vector gives desired result
+feature_names<-filter(features, logical_idx_of_mean_and_std)
+
+# give features better names
+#features_<-sapply(feature_names$V2, function(x) {
+#    gsub("^t|\-", '', x)
+#})
+
 # labels for the activities
-act_label<-read.table('uci/activity_labels.txt')
+act_label<-fread('uci/activity_labels.txt')
 
 # The processing for both test and train data will be the same so
 # compose a function to do those tasks.  Function will make use of the
@@ -44,9 +54,6 @@ manipulate_raw_data <-function(data, activity_data, subject_data) {
 
     data_final<-cbind(subject_data, with_activity)
 
-    # fix names on the data frame 
-    # here using logical vector gives desired result
-    feature_names<-filter(features, logical_idx_of_mean_and_std)
     # @TODO do some processing on the feature names to change
     # tBodyAcc-mean()-X to 'Mean Body Accel (x)' for example
     colnames(data_final)<-c("subject", "activity", feature_names$V2, recursive=T)
@@ -55,25 +62,28 @@ manipulate_raw_data <-function(data, activity_data, subject_data) {
 }
 
 # raw test data frame
-test<-read.table('uci/test/X_test.txt', comment.char='')
+test<-fread('uci/test/X_test.txt')
 
 # each row in test data frame is for these activities
-test_act<-read.table('uci/test/y_test.txt')
+test_act<-fread('uci/test/y_test.txt')
 
 # subject variable for each observation
-subjects<-read.table('uci/test/subject_test.txt')
+subjects<-fread('uci/test/subject_test.txt')
 
 # raw training data frame
-train<-read.table('uci/train/X_train.txt', comment.char='')
+train<-fread('uci/train/X_train.txt')
 
 # each row in training data frame is for these activities
-train_act<-read.table('uci/train/y_train.txt')
+train_act<-fread('uci/train/y_train.txt')
 
 # subject variable 
-train_subjects<-read.table('uci/train/subject_train.txt')
+train_subjects<-fread('uci/train/subject_train.txt')
 
 test_final<-manipulate_raw_data(test, test_act, subjects)
 train_final<-manipulate_raw_data(train, train_act, train_subjects)
 
 # combine training and test into single data frame
 out<-rbind(train_final, test_final)
+
+# Final data frame will have one observation for each combination of 
+# unique subject identifier and activity.  Looks like 180 rows (30 x 6).
