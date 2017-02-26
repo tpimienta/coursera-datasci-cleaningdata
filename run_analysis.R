ee4ffb039b981732c3481a1e578a29e607601dde
tom <tom@phpfunk.com>
2017-02-26 09:41:07 -0800

    Change back to read.csv...not sure if issue having data.table instances instead of data.frame.  Compute means for grouped data (step 5)

--- run_analysis.R
+++ run_analysis.R
@@ -7,7 +7,7 @@ library(data.table)
 # information that is common to processing both training and test data sets
 
 # the  variables represented in each column of the data frames
-features <- fread('uci/features.txt')
+features <- read.table('uci/features.txt', stringsAsFactors=F)
 
 # only want variables that are mean and standard deviation
 idx_of_mean_and_std <- grep("mean|std", features$V2, ignore.case=T)
@@ -25,7 +25,7 @@ feature_names<-filter(features, logical_idx_of_mean_and_std)
 #})
 
 # labels for the activities, we want factors, not character vectors!
-act_label<-fread('uci/activity_labels.txt', stringsAsFactors=T)
+act_label<-read.table('uci/activity_labels.txt', stringsAsFactors=T)
 
 # The processing for both test and train data will be the same so
 # compose a function to do those tasks.  Function will make use of the
@@ -62,22 +62,22 @@ manipulate_raw_data <-function(data, activity_data, subject_data) {
 }
 
 # raw test data frame
-test<-fread('uci/test/X_test.txt')
+test<-read.table('uci/test/X_test.txt')
 
 # each row in test data frame is for these activities
-test_act<-fread('uci/test/y_test.txt')
+test_act<-read.table('uci/test/y_test.txt')
 
 # subject variable for each observation
-subjects<-fread('uci/test/subject_test.txt')
+subjects<-read.table('uci/test/subject_test.txt')
 
 # raw training data frame
-train<-fread('uci/train/X_train.txt')
+train<-read.table('uci/train/X_train.txt')
 
 # each row in training data frame is for these activities
-train_act<-fread('uci/train/y_train.txt')
+train_act<-read.table('uci/train/y_train.txt')
 
 # subject variable 
-train_subjects<-fread('uci/train/subject_train.txt')
+train_subjects<-read.table('uci/train/subject_train.txt')
 
 test_final<-manipulate_raw_data(test, test_act, subjects)
 train_final<-manipulate_raw_data(train, train_act, train_subjects)
@@ -87,3 +87,8 @@ out<-rbind(train_final, test_final)
 
 # Final data frame will have one observation for each combination of 
 # unique subject identifier and activity.  Looks like 180 rows (30 x 6).
+final<-out %>% group_by(subject, activity) %>% summarize_all(mean)
+# we picked up lots of other classes along the way, I like output of data.frame
+final<-as.data.frame(final)
+
+# the names are wrong, prepend mean to each:
